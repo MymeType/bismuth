10862749bff26d6c63118985b49a707cd52a5e06
Alexandru Mihai Buzduc <109294126+MymeType@users.noreply.github.com>
2025-04-16 21:39:01 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,21 +9,23 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# COSMIC DE
+dnf -y install @cosmic-desktop-environment
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Universal Blue packages
+dnf -y copr enable ublue-os/packages
+#dnf5 -y copr enable ublue/os-akmods
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf -y install \
+    ublue-os-media-automount-udev \
+	ublue-{motd,fastfetch,bling,rebase-helper,setup-services,polkit-rules,brew,recipes} \
+	uupd \
+    kcm_ublue
 
-#### Example for enabling a System Unit File
+#dnf5 -y copr disable ublue/os-akmods
+dnf -y copr disable ublue-os/packages
 
-systemctl enable podman.socket
+#### Enable System Unit Files
+
+# Needed for cosmic-greeter
+systemctl enable greetd.service
